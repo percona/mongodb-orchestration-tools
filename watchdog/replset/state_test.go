bdffff14c786b9947eefaf53be015eb0c9854bfe
Tim Vaillancourt <tim@timvaillancourt.com>
2018-05-03 23:33:15 +0200

    Several watchdog/replset/state_test.go additions

--- watchdog/replset/state_test.go
+++ watchdog/replset/state_test.go
@@ -15,10 +15,13 @@
 package replset
 
 import (
+	"strconv"
+	"strings"
 	gotesting "testing"
 
 	testing "github.com/percona/dcos-mongo-tools/common/testing"
-	//"github.com/timvaillancourt/go-mongodb-replset/status"
+	rsConfig "github.com/timvaillancourt/go-mongodb-replset/config"
+	//rsStatus "github.com/timvaillancourt/go-mongodb-replset/status"
 )
 
 func TestNewState(t *gotesting.T) {
@@ -31,6 +34,54 @@ func TestNewState(t *gotesting.T) {
 	}
 	if state.doUpdate {
 		t.Error("replset.NewState() returned State struct with 'doUpdate' set to true")
+	}
+}
+
+func TestFetchConfig(t *gotesting.T) {
+	testing.DoSkipTest(t)
+
+	session, err := testing.GetPrimarySession(t)
+	if err != nil {
+		t.Fatalf("Database connection error: %s", err)
+	}
+	defer session.Close()
+
+	state := NewState(session, testing.MongodbReplsetName)
+	err = state.fetchConfig()
+	if err != nil {
+		t.Fatalf("state.fetchConfig() failed with error: %v", err)
+	}
+
+	if state.Config == nil {
+		t.Error("state.Config is nil")
+	} else if state.Config.Name != testing.MongodbReplsetName {
+		t.Errorf("state.Config.Name is not %s: %v", testing.MongodbReplsetName, state.Config.Name)
+	} else if len(state.Config.Members) <= 1 {
+		t.Error("state.Config.Members has <= 1 member")
+	}
+}
+
+func TestFetchStatus(t *gotesting.T) {
+	testing.DoSkipTest(t)
+
+	session, err := testing.GetPrimarySession(t)
+	if err != nil {
+		t.Fatalf("Database connection error: %s", err)
+	}
+	defer session.Close()
+
+	state := NewState(session, testing.MongodbReplsetName)
+	err = state.fetchStatus()
+	if err != nil {
+		t.Fatalf("state.fetchStatus() failed with error: %v", err)
+	}
+
+	if state.Status == nil {
+		t.Error("state.Status is nil")
+	} else if state.Status.Set != testing.MongodbReplsetName {
+		t.Errorf("state.Status.Set is not %s: %v", testing.MongodbReplsetName, state.Status.Set)
+	} else if len(state.Status.Members) <= 1 {
+		t.Error("state.Status.Members has <= 1 member")
 	}
 }
 
@@ -48,20 +99,40 @@ func TestFetch(t *gotesting.T) {
 	if err != nil {
 		t.Fatalf("state.Fetch() failed with error: %v", err)
 	}
+}
 
-	if state.Status == nil {
-		t.Error("state.Status is nil")
-	} else if state.Status.Set != testing.MongodbReplsetName {
-		t.Errorf("state.Status.Set is not %s: %v", testing.MongodbReplsetName, state.Status.Set)
-	} else if len(state.Status.Members) <= 1 {
-		t.Error("state.Status.Members has <= 1 member")
+func TestRemoveAddConfigMembers(t *gotesting.T) {
+	testing.DoSkipTest(t)
+
+	session, err := testing.GetPrimarySession(t)
+	if err != nil {
+		t.Fatalf("Database connection error: %s", err)
+	}
+	defer session.Close()
+
+	state := NewState(session, testing.MongodbReplsetName)
+	err = state.Fetch()
+	if err != nil {
+		t.Fatalf("state.Fetch() failed with error: %v", err)
 	}
 
-	if state.Config == nil {
-		t.Error("state.Config is nil")
-	} else if state.Config.Name != testing.MongodbReplsetName {
-		t.Errorf("state.Config.Name is not %s: %v", testing.MongodbReplsetName, state.Config.Name)
-	} else if len(state.Config.Members) <= 1 {
-		t.Error("state.Config.Members has <= 1 member")
+	removeMember := state.Config.Members[len(state.Config.Members)-1]
+	state.RemoveConfigMembers([]*rsConfig.Member{removeMember})
+	if state.doUpdate {
+		t.Errorf("state.doUpdate is true after state.RemoveConfigMembers()")
+	}
+
+	hostPort := strings.SplitN(removeMember.Host, ":", 2)
+	port, _ := strconv.Atoi(hostPort[1])
+	addMongod := &Mongod{
+		Host:          hostPort[0],
+		Port:          port,
+		Replset:       testing.MongodbReplsetName,
+		FrameworkName: "test",
+		PodName:       "mongo",
+	}
+	state.AddConfigMembers([]*Mongod{addMongod})
+	if state.doUpdate {
+		t.Errorf("state.doUpdate is true after state.AddConfigMembers()")
 	}
 }
