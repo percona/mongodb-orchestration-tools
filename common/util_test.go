63446b4622f2ddf9da10cf9c83c38b8937aee132
Tim Vaillancourt <tim@timvaillancourt.com>
2018-05-19 02:19:53 +0200

    Fix test

--- common/util_test.go
+++ common/util_test.go
@@ -69,8 +69,9 @@ func TestDoStopFalse(t *gotesting.T) {
 }
 
 func TestNewApp(t *gotesting.T) {
-	testApp := NewApp("testApp help", "git-commit-here", "branch-name-here")
+	testApp := NewApp("test help", "git-commit-here", "branch-name-here")
 	appModel := testApp.Model()
 	assert.Contains(t, appModel.Version, "common.test version "+dcosmongotools.Version+"\ngit commit git-commit-here, branch branch-name-here\ngo version go", "kingpin.Application version is unexpected")
 	assert.Equal(t, appAuthor, appModel.Author, "kingpin.Application author is unexpected")
+	assert.Equal(t, "test help", appModel.Help, "kingpin.Application help is unexpected")
 }
