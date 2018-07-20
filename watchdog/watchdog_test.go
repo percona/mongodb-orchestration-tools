48ce4429f90cec4f265b05467666f082cf891e00
Tim Vaillancourt <tim@timvaillancourt.com>
2018-07-20 19:37:09 +0200

    add some tests to watchdog (#136)

--- watchdog/watchdog_test.go
+++ watchdog/watchdog_test.go
@@ -13,3 +13,74 @@
 // limitations under the License.
 
 package watchdog
+
+import (
+	"os"
+	gotesting "testing"
+	"time"
+
+	//"github.com/percona/dcos-mongo-tools/common"
+	"github.com/percona/dcos-mongo-tools/common/api"
+	"github.com/percona/dcos-mongo-tools/common/api/mocks"
+	"github.com/percona/dcos-mongo-tools/common/db"
+	"github.com/percona/dcos-mongo-tools/common/logger"
+	"github.com/percona/dcos-mongo-tools/common/testing"
+	"github.com/percona/dcos-mongo-tools/watchdog/config"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	testWatchdog *Watchdog
+	testQuitChan = make(chan bool)
+	testConfig   = &config.Config{
+		FrameworkName: "test",
+		Username:      testing.MongodbAdminUser,
+		Password:      testing.MongodbAdminPassword,
+		APIPoll:       time.Millisecond * 100,
+		ReplsetPoll:   time.Millisecond * 100,
+		MetricsPort:   "65432",
+		SSL:           &db.SSLConfig{},
+	}
+	testAPIClient = &mocks.Client{}
+)
+
+func TestMain(m *gotesting.M) {
+	logger.SetupLogger(nil, logger.GetLogFormatter("test"), os.Stdout)
+	os.Exit(m.Run())
+}
+
+func TestWatchdogNew(t *gotesting.T) {
+	testWatchdog = New(testConfig, &testQuitChan, testAPIClient)
+	assert.NotNil(t, testWatchdog, ".New() returned nil")
+}
+
+func TestWatchdogDoIgnorePod(t *gotesting.T) {
+	testConfig.IgnorePods = []string{"ignore-me"}
+	assert.True(t, testWatchdog.doIgnorePod("ignore-me"))
+	assert.False(t, testWatchdog.doIgnorePod("dont-ignore-me"))
+}
+
+func TestWatchdogRun(t *gotesting.T) {
+	testAPIClient.On("GetPodURL").Return("http://test")
+	testAPIClient.On("GetPods").Return(&api.Pods{"test"}, nil)
+	testAPIClient.On("GetPodTasks", "test").Return([]api.PodTask{
+		&api.PodTaskHTTP{
+			Info: &api.PodTaskInfo{
+				//		Name: "test-mongod",
+				//		Command: &api.PodTaskCommand{
+				//		Environment: &api.PodTaskCommandEnvironment{
+				//		Variables: []*api.PodTaskCommandEnvironmentVariable{
+				//			{Name: common.EnvMongoDBPort, Value: testing.MongodbPrimaryPort},
+				//			{Name: common.EnvMongoDBReplset, Value: testing.MongodbReplsetName},
+				//		},
+				//	},
+				//	Value: "mongodb-executor-",
+				//},
+			},
+		},
+	}, nil)
+
+	go testWatchdog.Run()
+	time.Sleep(time.Millisecond * 150)
+	close(testQuitChan)
+}
