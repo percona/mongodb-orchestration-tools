542e82c3200351dc1854e0e8ec33df8495328fb7
Tim Vaillancourt <tim@timvaillancourt.com>
2018-07-23 20:09:10 +0200

    Add .mkdir() test (#140)
    * Add .mkdir() test
    
    * Use 1 x stat on test

--- executor/mongodb/mongod_test.go
+++ executor/mongodb/mongod_test.go
@@ -15,19 +15,23 @@
 package mongodb
 
 import (
+	"io/ioutil"
+	"os"
+	"os/user"
+	"strconv"
 	gotesting "testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 var (
-	testMongod *Mongod
-	testConfig = &Config{
-		ConfigDir: "/tmp",
-		BinDir:    "/usr/bin",
-		TmpDir:    "/tmp",
-		User:      "nobody",
-		Group:     "nogroup",
+	currentUser, _  = user.Current()
+	currentGroup, _ = user.LookupGroupId(currentUser.Gid)
+	testMongod      *Mongod
+	testConfig      = &Config{
+		BinDir: "/usr/bin",
+		User:   currentUser.Name,
+		Group:  currentGroup.Name,
 	}
 )
 
@@ -36,4 +40,27 @@ func TestExecutorMongoDBNewMongod(t *gotesting.T) {
 	assert.NotNil(t, testMongod, ".NewMongod() should not return nil")
 	assert.Contains(t, testMongod.commandBin, testConfig.BinDir)
 	assert.Contains(t, testMongod.configFile, testConfig.ConfigDir)
+}
+
+func TestExecutorMongoDBMkdir(t *gotesting.T) {
+	dir, _ := ioutil.TempDir("", "TestExecutorMongoDBMkdir")
+	os.RemoveAll(dir)
+	if _, err := os.Stat(dir); err == nil {
+		assert.FailNow(t, "dir should not exist before .mkdir()")
+	}
+
+	// bad uid + gid
+	assert.Error(t, mkdir(dir, 999999, 99999, 0777))
+
+	// good .mkdir()
+	uid, _ := strconv.Atoi(currentUser.Uid)
+	gid, _ := strconv.Atoi(currentGroup.Gid)
+	assert.NoError(t, mkdir(dir, uid, gid, DefaultDirMode))
+	defer os.RemoveAll(dir)
+
+	stat, err := os.Stat(dir)
+	if err != nil {
+		assert.FailNow(t, ".mkdir() did not create a directory")
+	}
+	assert.True(t, stat.IsDir())
 }
