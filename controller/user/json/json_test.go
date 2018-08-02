e7ced8c1e596fdb8fdbee91d77b8625d164bf795
Tim Vaillancourt <tim@timvaillancourt.com>
2018-08-02 18:32:50 +0200

    Rename test file

--- controller/user/json/json_test.go
+++ controller/user/json/json_test.go
@@ -26,7 +26,7 @@ import (
 const (
 	testUserFile                   = "test/test-user.json"
 	testUserCLIPayloadFile         = "test/test-user.json.base64"
-	testUserCLIPayloadFileIssue218 = "test/test-user-broken_issue218.json.base64"
+	testUserCLIPayloadFileIssue218 = "test/test-user-issue218.json.base64"
 	testUserFileBroken             = "test/test-user-broken.json"
 )
 
