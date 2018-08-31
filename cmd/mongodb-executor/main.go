ebb1b4838d154631375b77a7a11013ac48fd9dc9
Tim Vaillancourt <tim@timvaillancourt.com>
2018-08-31 19:11:04 +0200

    Make cmd wait on signals or quit chan

--- cmd/mongodb-executor/main.go
+++ cmd/mongodb-executor/main.go
@@ -231,9 +231,15 @@ func main() {
 	// wait for signals from the OS
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-	sig := <-signals
-	log.Infof("Received %s signal, killing %s daemon and jobs", sig, cnf.NodeType)
 
-	// send quit to all goroutines
-	quit <- true
+	select {
+	case <-quit:
+		log.Infof("Received quit signal from daemon")
+		return
+	case sig := <-signals:
+		log.Infof("Received %s signal, killing %s daemon and jobs", sig, cnf.NodeType)
+		// send quit to all goroutines
+		quit <- true
+		return
+	}
 }
