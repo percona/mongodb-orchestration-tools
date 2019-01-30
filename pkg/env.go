0f6bea4e698eb49f6c18f92c7f2197d2729a5f1b
Tim Vaillancourt <tim@timvaillancourt.com>
2019-01-30 15:57:46 +0100

    Fix test

--- pkg/env.go
+++ pkg/env.go
@@ -17,6 +17,7 @@ package pkg
 const (
 	// general
 	EnvLogVerbose     = "LOG_VERBOSE"
+	EnvServiceName    = "SERVICE_NAME"
 	EnvMongoDBPort    = "MONGODB_PORT"
 	EnvMongoDBIp      = "MONGODB_IP"
 	EnvMongoDBReplset = "MONGODB_REPLSET"
